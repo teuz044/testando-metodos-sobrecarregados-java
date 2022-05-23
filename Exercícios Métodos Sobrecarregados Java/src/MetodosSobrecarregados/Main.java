a754f02fc3afdc48463491e9efbde699bb7a74b2
teuz044 <matheus.gabriel80@hotmail.com>
2022-05-23 16:25:23 -0300

    Métodos Sobrecarregados em Java, continuação do exercício anterior.

---
+++ Exercícios Métodos Sobrecarregados Java/src/MetodosSobrecarregados/Main.java
@@ -0,0 +1,16 @@
+package MetodosSobrecarregados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Quadrilatero.area(5);
+		Quadrilatero.area(3, 5);
+		Quadrilatero.area(10, 7, 8);
+		Quadrilatero.area(15,8);
+		
+		
+
+	}
+
+}
